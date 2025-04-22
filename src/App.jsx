0c9a8dbbdf1947ad8713f693f3e31764981b4ece
Emil <kazimli_187@mail.ru>
2025-04-22 14:32:08 +0400

    "Creating routers"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Router, Routes, Route} from 'react-router-dom'
+import Users from './pages/users'
+import Contact from './pages/contact'
+import Home from './pages/home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/users" element={<Users/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
     </>
   )
 }
